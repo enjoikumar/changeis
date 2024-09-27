45c701c87508be1d274f7f34ee8673c77e4344bb
enjoikumar <kamalkumar1027@yahoo.com>
2024-09-26 20:53:26 -0400

    added useEffect hook to get data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
+
+  let [changeData, setChangeData] = useState();
+
+  useEffect(() => {
+    fetch("https://fakerapi.it/api/v1/images?_width=380")
+      .then(response => response.json())
+      .then(data => setChangeData(data))
+
+    console.log(changeData)
+  }, [])
+
   return (
     <div className="App">
       <Card />
+
     </div>
   );
 }
