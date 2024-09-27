032019258784d5a4a1d47d68272d9451a6cca0fd
enjoikumar <kamalkumar1027@yahoo.com>
2024-09-26 21:00:31 -0400

    fixed use effect call

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 import Card from './components/Card';
 
 function App() {
-
-  let [changeData, setChangeData] = useState();
+  let [changeData, setChangeData] = useState([]);
 
   useEffect(() => {
     fetch("https://fakerapi.it/api/v1/images?_width=380")
       .then(response => response.json())
-      .then(data => setChangeData(data))
-
-    console.log(changeData)
-  }, [])
+      .then(data => {
+        setChangeData(data.data);
+        console.log(changeData);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <div className="App">
-      <Card />
-
+      card component will go here
+      {/* <Card data={changeData} /> */}
     </div>
   );
 }
